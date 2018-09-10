1a050f6b5221107ddc9451f0106d6c0bb8fbd5fb
pengkr <1009870674@qq.com>
2018-09-10 19:41:50 +0800

    First test commit.

--- app/src/main/java/com/pengkr/testproject/TestActivity.java
+++ app/src/main/java/com/pengkr/testproject/TestActivity.java
@@ -3,15 +3,11 @@ package com.pengkr.testproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    private void test(){
-
+        setContentView(R.layout.activity_test);
     }
 }
